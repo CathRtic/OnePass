86e8adc0b784888254f09263b83b940aeded2bfe
CathRtic <nikunj2077@outlook.com>
2024-05-14 12:16:31 -0400

    Improved Randomness Security

--- src/password_generator.py
+++ src/password_generator.py
@@ -1,8 +1,8 @@
-import random
+import secrets
 import string
 
 def generate_password(length):
     characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for _ in range(length))
+    password = ''.join(secrets.choice(characters) for _ in range(length))
     return password
 
